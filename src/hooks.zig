95a866a437e4d5653ef92b99e9242c31dfa36ab0
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2026-04-30 22:49:07 +0300

    Track d3d11.

--- src/hooks.zig
+++ src/hooks.zig
@@ -7,7 +7,16 @@ const mem = std.mem;
 var load_library_a: *@TypeOf(LoadLibraryA) = undefined;
 var load_library_w: *@TypeOf(LoadLibraryW) = undefined;
 
+var thread: windows.HANDLE = undefined;
+
 pub fn init() !void {
+    thread = windows.GetCurrentThread();
+
+    // todo: ping unavtive hooks
+    if (windows.GetModuleHandle("d3d11.dll")) |_| {
+        windows.OutputDebugString("seen d3d11.dll");
+    }
+
     try detours.TransactionBegin();
     errdefer detours.TransactionAbort() catch {};
 
@@ -16,16 +25,10 @@ pub fn init() !void {
     load_library_a = @ptrCast(try windows.GetProcAddress(kernel32, "LoadLibraryA"));
     load_library_w = @ptrCast(try windows.GetProcAddress(kernel32, "LoadLibraryW"));
 
-    std.debug.print("000: load_library_a: {p}\n", .{load_library_a});
-    std.debug.print("000: load_library_w: {p}\n", .{load_library_w});
-
     try detours.Attach(LoadLibraryA, &load_library_a);
     try detours.Attach(LoadLibraryW, &load_library_w);
 
     try detours.TransactionCommit();
-
-    std.debug.print("111: load_library_a: {p}\n", .{load_library_a});
-    std.debug.print("111: load_library_w: {p}\n", .{load_library_w});
 }
 
 pub fn deinit() !void {
@@ -41,13 +44,23 @@ pub fn deinit() !void {
 // if we will hook smth we should update thread from here
 fn LoadLibraryA(lpLibFileName: windows.LPCSTR) callconv(.winapi) ?windows.HMODULE {
     const lib = load_library_a(lpLibFileName) orelse return null;
-    windows.OutputDebugString(mem.span(lpLibFileName));
+
+    // todo: ping unavtive hooks
+    if (windows.GetModuleHandle("d3d11.dll")) |_| {
+        windows.OutputDebugString("seen d3d11.dll");
+    }
+
     return lib;
 }
 
 // if we will hook smth we should update thread from here
 fn LoadLibraryW(lpLibFileName: windows.LPCWSTR) callconv(.winapi) ?windows.HMODULE {
     const lib = load_library_w(lpLibFileName) orelse return null;
-    windows.OutputDebugStringW(mem.span(lpLibFileName));
+
+    // todo: ping unavtive hooks
+    if (windows.GetModuleHandle("d3d11.dll")) |_| {
+        windows.OutputDebugString("seen d3d11.dll");
+    }
+
     return lib;
 }
